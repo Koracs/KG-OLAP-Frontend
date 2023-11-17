d151cfc7bc06ea63cff38c5dc9ef94fc632a4a99
Koracs <115572388+Koracs@users.noreply.github.com>
2023-11-17 09:27:56 +0100

    add test data and change docker-compose.yml

--- frontend/app/query/queryaction.js
+++ frontend/app/query/queryaction.js
@@ -1,12 +1,31 @@
 "use server";
 
 import {redirect} from "next/navigation";
+import rdfParser from "rdf-parse";
+import * as fs from "fs";
+import N3 from "n3";
+import {prefixes} from "n3/lib/N3Util";
+
 
 export const serverAction = async (query) => {
     try {
         const text = query.get("queryInput")?.valueOf();
         //throw new Error("Query Endpoint not implemented");
+        const textStream = fs.createReadStream("./testData/1.nq");
 
+        /*rdfParser.parse(textStream, { contentType: "application/n-quads", baseIRI: "http://example.org/bigkgolap/" })
+            .on("data", (quad) => console.log(quad))
+            .on("error", (error) => console.error(error))
+            .on("end", () => console.log("All done!"));
+        */
+
+        const parser = new N3.Parser({ format: 'N-Quads', baseIRI: 'http://example.org/bigkgolap/' });
+        parser.parse(textStream, (error, quad, prefixes) => {
+            if (quad)
+                console.log(quad);
+            else
+                console.log("# That's all, folks!", prefixes);
+        });
 
     } catch (error) {
         console.error(error);
@@ -14,5 +33,5 @@ export const serverAction = async (query) => {
             error: error.message,
         }
     }
-    redirect("/query/result")
+    //redirect("/query/result")
 }
