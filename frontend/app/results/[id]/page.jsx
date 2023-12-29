2a31fc900c0ff2cbe955d25cc4abfdc90019fcb5
Koracs <115572388+Koracs@users.noreply.github.com>
2023-12-29 20:01:02 +0100

    add result overview page

--- frontend/app/results/[id]/page.jsx
+++ frontend/app/results/[id]/page.jsx
@@ -7,6 +7,9 @@ import {deleteResult, updateResult} from "@/app/lib/resultaction";
 export default async function ResultPage({params}) {
     const result = await prisma.QueryResult.findUnique({where: {id: params?.id}});
 
+    const quad_count = result?.quad_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    const context_count = result?.context_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    const total_time = (result?.ts_end - result?.ts_start) / 1000;
     async function deleteAction() {
         "use server"
         await deleteResult(params?.id);
@@ -28,24 +31,39 @@ export default async function ResultPage({params}) {
                 }
             ]}/>
             <h1>{params?.id}</h1>
+            <br/>
+            <h2>Query Results</h2>
             <div className={"result"}>
-                <p className={"result-query"}><b>Query String:</b><br/>{result?.queryText}</p>
-                <span className={"result-links"}>
-                    <Link className={"button resultitem-table"} href={`/results/${result?.id}/table`}>Show Table</Link>
-                    <Link className={"button resultitem-contexts"} href={`/results/${result?.id}/contexts`}>Show
+                <div className={"result-text"}>
+                    <p><b>Query String:</b><br/>{result?.queryText}</p>
+                    <br/>
+                    <p><b>Quad Count:</b> {quad_count}</p>
+                    <p><b>Total Contexts:</b> {context_count}</p>
+                </div>
+                <div className={"result-links"}>
+                    <Link className={"button result-link"} href={`/results/${result?.id}/table`}>Show Table</Link>
+                    <Link className={"button result-link"} href={`/results/${result?.id}/contexts`}>Show
                         Contexts</Link>
-                    <Link className={"button resultitem-metrics"} href={`/results/${result?.id}/metrics`}>Show
-                        Metrics</Link>
-                </span>
+                </div>
             </div>
+            <br/>
+            <h2>Query Metrics</h2>
             <div className={"result"}>
-                <p><b>Created:</b>{result?.createdAt.toLocaleString()}</p>
+                <div className={"result-text"}>
+                <p><b>Created:</b> {result?.createdAt.toLocaleString()}</p>
                 <p><b>Last Update:</b> {result?.updatedAt.toLocaleString()}</p>
-                <span className={"result-links"}>
-                    <button formAction={rerunAction} className={"button resultitem-rerun"}>Re-Run Query</button>
-                    <button formAction={deleteAction} className={"button error-button resultitem-delete"}>Delete</button>
-                </span>
+                <p><b>Total Time (seconds):</b> {total_time}</p>
+                </div>
+                <div className={"result-links"}>
+                                        <Link className={"button result-link"}
+                                              href={`/results/${result?.id}/metrics`}>Show
+                        Metrics</Link>
+                </div>
             </div>
+            <br/>
+            <button formAction={rerunAction} className={"button result-link"}>Re-Run Query</button>
+            <button formAction={deleteAction}
+                    className={"button error-button result-link"}>Delete Result</button>
         </>
     )
 }
