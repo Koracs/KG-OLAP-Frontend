0c6b4d61f6f0bb8cb0a44bda99d5cf19ce541325
Koracs <115572388+Koracs@users.noreply.github.com>
2024-02-14 15:18:47 +0100

    fix landing page display bug

--- components/landingpage/LandingResultItem.jsx
+++ components/landingpage/LandingResultItem.jsx
@@ -1,12 +1,13 @@
 export default function LandingResultItem() {
     const uuid = "My-Latest-Query-Result";
     const queryText = "SELECT time_month=2000-01 AND location_territory=France ROLLUP ON topic_all, location_all, time_all";
+    const date = new Date();
 
     return (
         <div className={"resultitem-container"}>
             <span className={"resultitem-uuid"} href={`/results/${uuid}`}>{uuid}</span>
             <span className={"resultitem-queryText"}><b>Query:</b> {queryText}</span>
-            <span className={"resultitem-lastUpdate"}><b>Last Update:</b> {Date.now().toLocaleString()}</span>
+            <span className={"resultitem-lastUpdate"}><b>Last Update:</b> {date.toLocaleString()}</span>
             <button className={"button resultitem-table"}>Show Table</button>
             <button className={"button resultitem-contexts"}>Show Contexts</button>
             <button className={"button resultitem-metrics"}>Show Metrics</button>
